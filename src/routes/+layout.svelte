91508e82801f4460fba48e131b79277be52f0172
Andy <AndyTn9@gmail.com>
2025-12-17 21:15:54 -0500

    Addition of navigation bar, necessary routes for the application, and the foundational packages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,28 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+	import { fly } from 'svelte/transition';
 
 	let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 {@render children()}
+
+<Navbar>
+	<NavBrand href="/">
+		<span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+			Andy Then
+		</span>
+	</NavBrand>
+
+	<NavHamburger />
+
+	<NavUl transition={fly} transitionParams={{ y: -20, duration: 250 }}>
+		<NavLi href="/experience">Experience</NavLi>
+		<NavLi href="/projects">Projects</NavLi>
+		<NavLi href="/posts">Posts</NavLi>
+		<NavLi href="/research">Research</NavLi>
+	</NavUl>
+</Navbar>
---
+++ src/routes/experience/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { Alert } from 'flowbite-svelte';
+</script>
+
+<Alert color="blue">
+	<span class="font-medium">Info:</span>
+	This page is currently under construction
+</Alert>
